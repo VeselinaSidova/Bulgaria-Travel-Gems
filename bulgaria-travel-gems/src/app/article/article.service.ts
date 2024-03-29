88fbde867ca8cbf8a887edd0a4ecb869c44583a0
Veselina Sidova <vessy.sidova@gmail.com>
2024-03-30 00:40:41 +0200

    added ArticleList

--- bulgaria-travel-gems/src/app/article/article.service.ts
+++ bulgaria-travel-gems/src/app/article/article.service.ts
@@ -1,59 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Article } from '../types/article';
+import { ApiService } from '../api.service';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  private apiUrl = 'http://localhost:3030/data/articles';
+  private baseUrl = environment.baseUrl;
+  private apiUrl = `${this.baseUrl}/data/articles`;
 
-  constructor(private http: HttpClient) {}
-
-  private getAuthToken(): string | null {
-    return localStorage.getItem('authToken');
-  }
-
-  private getHeaders(): HttpHeaders {
-    let headers = new HttpHeaders().set('Content-Type', 'application/json');
-    const token = this.getAuthToken();
-    console.log('Auth Token:', token); // This will log the token to the console
-    if (token) {
-      headers = headers.set(
-        'X-Authorization',
-        '17179c4b5486d5d92f2a14bc556fdea897f18f0c9a0558642cd09f894acc48e1'
-      );
-    }
-    return headers;
-  }
+  constructor(private apiService: ApiService) {}
 
   getArticles(): Observable<Article[]> {
-    return this.http.get<Article[]>(this.apiUrl, {
-      headers: this.getHeaders(),
-    });
-  }
-
-  getArticleById(id: number): Observable<Article> {
-    return this.http.get<Article>(`${this.apiUrl}/${id}`, {
-      headers: this.getHeaders(),
-    });
-  }
-
-  createArticle(article: Article): Observable<Article> {
-    return this.http.post<Article>(this.apiUrl, article, {
-      headers: this.getHeaders(),
-    });
-  }
-
-  updateArticle(article: Article): Observable<Article> {
-    return this.http.put<Article>(
-      `${this.apiUrl}/articles/${article.id}`,
-      article
-    );
-  }
-
-  deleteArticle(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/articles/${id}`);
+    return this.apiService.request('GET', this.apiUrl);
   }
 }
--- bulgaria-travel-gems/src/app/article/articles-list/articles-list.component.ts
+++ bulgaria-travel-gems/src/app/article/articles-list/articles-list.component.ts
@@ -7,8 +7,25 @@ import { ArticleService } from '../article.service';
   templateUrl: './articles-list.component.html',
   styleUrls: ['./articles-list.component.css'],
 })
-export class ArticlesListComponent {
-  articles: Article[] | null = [];
+export class ArticlesListComponent implements OnInit {
+  articles: Article[] | null = null;
 
   constructor(private articleService: ArticleService) {}
+
+  ngOnInit(): void {
+    this.getArticles();
+  }
+
+  getArticles(): void {
+    this.articleService.getArticles().subscribe({
+      next: (articles) => {
+        this.articles = articles;
+        console.log('Articles fetched successfully:', articles); // Add this line to print the articles
+      },
+      error: (error) => {
+        console.error('Error fetching articles:', error);
+        this.articles = [];
+      },
+    });
+  }
 }
