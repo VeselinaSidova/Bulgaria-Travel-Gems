b885536bcf00932499b3f270493affb9e1006d27
Veselina Sidova <vessy.sidova@gmail.com>
2024-03-30 17:56:21 +0200

    added login form validation and errors

--- bulgaria-travel-gems/src/app/user/login/login.component.ts
+++ bulgaria-travel-gems/src/app/user/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { emailValidator } from 'src/app/shared/utils/email-validator';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  form = this.fb.group({
+    email: ['', [Validators.required, emailValidator()]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  login(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value);
+  }
 }
