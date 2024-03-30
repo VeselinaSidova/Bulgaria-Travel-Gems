242604634c90598069ee55b0e77750db886ce610
Veselina Sidova <vessy.sidova@gmail.com>
2024-03-30 17:14:12 +0200

    added routing

--- bulgaria-travel-gems/src/app/article/article-routing.module.ts
+++ bulgaria-travel-gems/src/app/article/article-routing.module.ts
@@ -4,18 +4,32 @@ import { ArticlesListComponent } from './articles-list/articles-list.component';
 import { AddArticleComponent } from './add-article/add-article.component';
 import { MyArticlesComponent } from './my-articles/my-articles.component';
 import { LikedArticlesComponent } from './liked-articles/liked-articles.component';
+import { AuthActivate } from '../guards/auth.activate';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
 
 const routes: Routes = [
   {
     path: 'articles',
     children: [
       { path: '', pathMatch: 'full', component: ArticlesListComponent },
-      { path: ':_id', component: ArticlesListComponent },
+      { path: ':_id', component: ArticleDetailsComponent },
     ],
   },
-  { path: 'add-article', component: AddArticleComponent },
-  { path: 'my-articles', component: MyArticlesComponent },
-  { path: 'liked-articles', component: LikedArticlesComponent },
+  {
+    path: 'add-article',
+    component: AddArticleComponent,
+    //canActivate: [AuthActivate],
+  },
+  {
+    path: 'my-articles',
+    component: MyArticlesComponent,
+    canActivate: [AuthActivate],
+  },
+  {
+    path: 'liked-articles',
+    component: LikedArticlesComponent,
+    canActivate: [AuthActivate],
+  },
 ];
 
 @NgModule({
