af5296d9e46d9d1d9e71dc09e6950786051a579d
Veselina Sidova <vessy.sidova@gmail.com>
2024-03-30 17:16:13 +0200

    test authentication

--- bulgaria-travel-gems/src/app/user/user.service.ts
+++ bulgaria-travel-gems/src/app/user/user.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { User } from '../types/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  user: User | undefined;
+  USER_KEY = '[user]';
 
-  constructor() { }
+  get isLogged(): boolean {
+    return !!this.user;
+  }
+
+  constructor() {
+    try {
+      const lsUser = localStorage.getItem(this.USER_KEY) || '';
+      this.user = JSON.parse(lsUser);
+    } catch (error) {
+      this.user = undefined;
+    }
+  }
+
+  login() {
+    this.user = {
+      firstName: 'Peter',
+      lastName: 'Ivanov',
+      email: 'peter@abv.bg',
+      password: '123456',
+    };
+
+    localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.removeItem(this.USER_KEY);
+  }
 }
