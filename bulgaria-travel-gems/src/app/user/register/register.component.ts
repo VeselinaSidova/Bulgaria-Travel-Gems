f1a2051fc581e60793887809cbf4203d317875b2
Veselina Sidova <vessy.sidova@gmail.com>
2024-03-30 17:15:52 +0200

    added register form validation and error messages

--- bulgaria-travel-gems/src/app/user/register/register.component.ts
+++ bulgaria-travel-gems/src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { emailValidator } from 'src/app/shared/utils/email-validator';
+import { matchPasswordsValidator } from 'src/app/shared/utils/match-passwords.validator';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-  register(): void {}
+  form = this.fb.group({
+    firstName: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(20)],
+    ],
+    lastName: [
+      '',
+      [Validators.required, Validators.minLength(2), Validators.maxLength(20)],
+    ],
+    email: ['', [Validators.required, emailValidator()]],
+    passGroup: this.fb.group(
+      {
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        rePassword: ['', [Validators.required, Validators.minLength(6)]],
+      },
+      {
+        validators: [matchPasswordsValidator('password', 'rePassword')],
+      }
+    ),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  register(): void {
+    if (this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value);
+  }
 }
