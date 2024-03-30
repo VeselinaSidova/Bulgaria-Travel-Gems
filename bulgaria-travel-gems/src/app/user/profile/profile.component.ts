2d6cf669fd992925ab3a34798a4b326bfa34f646
Veselina Sidova <vessy.sidova@gmail.com>
2024-03-30 11:59:04 +0200

    changed user

--- bulgaria-travel-gems/src/app/user/profile/profile.component.ts
+++ bulgaria-travel-gems/src/app/user/profile/profile.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/types/user';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  user: User = {
+    firstName: 'Peter',
+    lastName: 'Ivanov',
+    email: 'peter@abv.bg',
+    password: '123456',
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
