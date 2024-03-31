ae6ef31719a20eca2da648e37b902da44351a229
Veselina Sidova <vessy.sidova@gmail.com>
2024-03-31 22:40:02 +0300

    added dynamic profile

--- bulgaria-travel-gems/src/app/user/profile/profile.component.ts
+++ bulgaria-travel-gems/src/app/user/profile/profile.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { UserService } from 'src/app/user/user.service';
 import { User } from 'src/app/types/user';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent implements OnInit {
-  user: User = {
-    firstName: 'Peter',
-    lastName: 'Ivanov',
-    email: 'peter@abv.bg',
-    password: '123456',
-  };
+export class ProfileComponent implements OnInit, OnDestroy {
+  user: Omit<User, 'password'> | null = null;
+  private subscription!: Subscription; // Added definite assignment assertion here
 
-  constructor() {}
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscription = this.userService.authState$.subscribe((authState) => {
+      this.user = authState.user;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
