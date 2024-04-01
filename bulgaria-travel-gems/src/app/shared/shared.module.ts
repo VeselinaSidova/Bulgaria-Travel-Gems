8ce7f75738547c1c23759ad58667c9b006139888
Veselina Sidova <vessy.sidova@gmail.com>
2024-04-01 16:11:16 +0300

    added pipes

--- bulgaria-travel-gems/src/app/shared/shared.module.ts
+++ bulgaria-travel-gems/src/app/shared/shared.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeartToggleComponent } from './heart-toggle/heart-toggle.component';
+import { SlicePipe } from './pipes/slice.pipe';
+import { ElapsedTimePipe } from './pipes/elapsed-time.pipe';
+import { LocationNameFromIdPipe } from './pipes/location-name-from-id.pipe';
 
 @NgModule({
-  declarations: [HeartToggleComponent],
+  declarations: [
+    HeartToggleComponent,
+    SlicePipe,
+    ElapsedTimePipe,
+    LocationNameFromIdPipe,
+  ],
   imports: [CommonModule],
-  exports: [HeartToggleComponent],
+  exports: [
+    HeartToggleComponent,
+    SlicePipe,
+    ElapsedTimePipe,
+    LocationNameFromIdPipe,
+  ],
 })
 export class SharedModule {}
