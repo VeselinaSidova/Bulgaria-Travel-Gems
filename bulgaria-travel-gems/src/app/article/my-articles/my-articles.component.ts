8d64660e11974f17521c7989d5df8c6f4dc46633
Veselina Sidova <vessy.sidova@gmail.com>
2024-04-02 00:32:06 +0300

    added my articled functionality

--- bulgaria-travel-gems/src/app/article/my-articles/my-articles.component.ts
+++ bulgaria-travel-gems/src/app/article/my-articles/my-articles.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Article } from 'src/app/types/article';
+import { ArticleService } from '../article.service';
 
 @Component({
   selector: 'app-my-articles',
   templateUrl: './my-articles.component.html',
-  styleUrls: ['./my-articles.component.css']
+  styleUrls: ['./my-articles.component.css'],
 })
-export class MyArticlesComponent {
+export class MyArticlesComponent implements OnInit {
+  myArticles$!: Observable<Article[]>;
+  hasArticles = false;
+  currentUser: any;
 
+  constructor(private articleService: ArticleService) {}
+
+  ngOnInit(): void {
+    const userDataJson = localStorage.getItem('user_data');
+    if (userDataJson) {
+      this.currentUser = JSON.parse(userDataJson);
+    }
+
+    if (this.currentUser?.id) {
+      this.myArticles$ = this.articleService.getArticles().pipe(
+        map((articles) => {
+          const userArticles = articles.filter(
+            (article) => article._ownerId === this.currentUser.id
+          );
+          this.hasArticles = userArticles.length > 0;
+          return userArticles;
+        })
+      );
+    }
+  }
 }
