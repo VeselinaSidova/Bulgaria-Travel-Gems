9c2465fc9b62ca6a78e6e5f5f51a1250622aa4a6
Veselina Sidova <vessy.sidova@gmail.com>
2024-04-02 03:31:23 +0300

    implemented like functionality

--- bulgaria-travel-gems/src/app/article/liked-articles/liked-articles.component.ts
+++ bulgaria-travel-gems/src/app/article/liked-articles/liked-articles.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { Article } from 'src/app/types/article';
+import { ArticleService } from '../article.service';
+import { UserService } from 'src/app/user/user.service';
+import { User } from 'src/app/types/user';
 
 @Component({
   selector: 'app-liked-articles',
   templateUrl: './liked-articles.component.html',
-  styleUrls: ['./liked-articles.component.css']
+  styleUrls: ['./liked-articles.component.css'],
 })
-export class LikedArticlesComponent {
+export class LikedArticlesComponent implements OnInit {
+  likedArticles$: Observable<Article[]> | undefined;
+  currentUser: Omit<User, 'password'> | null = null;
 
+  constructor(
+    private articleService: ArticleService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.userService.authState$.subscribe({
+      next: (authState) => {
+        this.currentUser = authState.user;
+        if (this.currentUser && this.currentUser.likedArticles) {
+          this.likedArticles$ = this.articleService
+            .getArticles()
+            .pipe(
+              map((articles) =>
+                articles.filter((article) =>
+                  this.currentUser?.likedArticles?.includes(article._id!)
+                )
+              )
+            );
+        }
+      },
+      error: (error) => console.error('Error fetching liked articles:', error),
+    });
+  }
+
+  onToggleLike(articleId: string): void {
+    this.userService.toggleLikedArticle(articleId).subscribe({
+      next: (user) => {},
+      error: (error) => console.error('Error toggling liked article:', error),
+    });
+  }
 }
--- bulgaria-travel-gems/src/app/user/user.service.ts
+++ bulgaria-travel-gems/src/app/user/user.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../api.service';
 import { environment } from 'src/environments/environment.development';
-import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, of, tap, throwError } from 'rxjs';
 import { User } from '../types/user';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -22,7 +22,7 @@ export class UserService {
   AUTH_TOKEN_KEY = 'auth_token';
   USER_DATA_KEY = 'user_data';
 
-  constructor(private apiService: ApiService, private http: HttpClient) {
+  constructor(private apiService: ApiService, private router: Router) {
     this.initializeAuthState();
   }
 
@@ -83,5 +83,37 @@ export class UserService {
       .pipe(
         tap(() => this.authState$$.next({ isLoggedIn: false, user: null }))
       );
+  }
+
+  toggleLikedArticle(articleId: string): Observable<any> {
+    const userDataJson = localStorage.getItem(this.USER_DATA_KEY);
+    const token = localStorage.getItem(this.AUTH_TOKEN_KEY);
+    if (!userDataJson || !token) {
+      this.router.navigate(['/login']);
+      return throwError(() => new Error('User not found'));
+    }
+
+    const userData: User = JSON.parse(userDataJson);
+
+    const isLiked = userData.likedArticles?.includes(articleId);
+
+    if (isLiked) {
+      userData.likedArticles = userData.likedArticles?.filter(
+        (id) => id !== articleId
+      );
+    } else {
+      userData.likedArticles = [...(userData.likedArticles || []), articleId];
+    }
+
+    localStorage.setItem(this.USER_DATA_KEY, JSON.stringify(userData));
+
+    this.authState$$.next({ isLoggedIn: true, user: userData });
+
+    return this.apiService.request(
+      'PUT',
+      `${environment.baseUrl}/users/${userData.id}`,
+      userData,
+      token
+    );
   }
 }
