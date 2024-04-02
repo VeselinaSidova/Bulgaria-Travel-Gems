bca53a019cac83c87be1f16224c7052c1cde1bc9
Veselina Sidova <vessy.sidova@gmail.com>
2024-04-02 20:51:20 +0300

    added my locations functionality and template

--- bulgaria-travel-gems/src/app/location/my-locations/my-locations.component.ts
+++ bulgaria-travel-gems/src/app/location/my-locations/my-locations.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { LocationService } from '../location.service';
+import { UserService } from 'src/app/user/user.service';
+import { Location } from 'src/app/types/location';
 
 @Component({
   selector: 'app-my-locations',
   templateUrl: './my-locations.component.html',
-  styleUrls: ['./my-locations.component.css']
+  styleUrls: ['./my-locations.component.css'],
 })
-export class MyLocationsComponent {
+export class MyLocationsComponent implements OnInit {
+  myLocations$!: Observable<Location[]>;
+  hasLocations = false;
+  currentUser: any;
 
+  constructor(
+    private locationService: LocationService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    const userDataJson = localStorage.getItem('user_data');
+    if (userDataJson) {
+      this.currentUser = JSON.parse(userDataJson);
+    }
+
+    if (this.currentUser?.id) {
+      this.myLocations$ = this.locationService.getLocations().pipe(
+        map((locations) => {
+          const userLocations = locations.filter(
+            (location) => location._ownerId === this.currentUser.id
+          );
+          this.hasLocations = userLocations.length > 0;
+          return userLocations;
+        })
+      );
+    }
+  }
 }
