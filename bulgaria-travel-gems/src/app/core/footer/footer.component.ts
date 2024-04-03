9f025e98781187afa62eef3f217a864cc7151f05
Veselina Sidova <vessy.sidova@gmail.com>
2024-04-03 19:53:37 +0300

    modified footer visibility

--- bulgaria-travel-gems/src/app/core/footer/footer.component.ts
+++ bulgaria-travel-gems/src/app/core/footer/footer.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  footerVisible = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.checkFooterVisibility();
+      });
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkFooterVisibility();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.checkFooterVisibility();
+  }
+
+  private checkFooterVisibility() {
+    const contentHeight = document.body.scrollHeight;
+    const viewportHeight = window.innerHeight;
+    const bottomOfPage = contentHeight - viewportHeight;
+    const scrolled = window.scrollY;
+
+    this.footerVisible =
+      contentHeight <= viewportHeight
+        ? scrolled >= bottomOfPage
+        : scrolled + viewportHeight >= contentHeight;
+  }
 }
