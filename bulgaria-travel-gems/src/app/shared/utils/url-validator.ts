0e93082a6bb1051943fb7e987f30913cba339901
Veselina Sidova <vessy.sidova@gmail.com>
2024-04-03 18:08:14 +0300

    update url regex

--- bulgaria-travel-gems/src/app/shared/utils/url-validator.ts
+++ bulgaria-travel-gems/src/app/shared/utils/url-validator.ts
@@ -1,7 +1,8 @@
 import { ValidatorFn, AbstractControl } from '@angular/forms';
 
 export function urlValidator(): ValidatorFn {
-  const regExp = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/;
+  const regExp =
+    /^(?:(?:https?|ftp):\/\/)?(?:www\.)?[a-z0-9-]+(?:\.[a-z0-9-]+)+[^\s]*$/i;
 
   return (control: AbstractControl) => {
     const isUrlValid = regExp.test(control.value);
